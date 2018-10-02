861fb78278b546e92ad20224765463cea8a08dd9
Christina Gislason <christinamkcho@gmail.com>
2018-10-02 14:18:49 -0700

    Add project files.

---
+++ ExampleConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
